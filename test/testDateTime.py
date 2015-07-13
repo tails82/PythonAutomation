9ac647f62adf8d15f462f0f6ffba505ee485ce37
Tails.Zhang <tails.zhang@ef.com>
2015-07-13 16:06:35 +0800

    test date time

--- test/testDateTime.py
+++ test/testDateTime.py
@@ -1,5 +1,11 @@
 __author__ = 'Tails'
 
 import time
+import datetime
 
 print time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time()))
+
+today = datetime.datetime.now()
+date = today + datetime.timedelta(days=2)
+
+print date - today
